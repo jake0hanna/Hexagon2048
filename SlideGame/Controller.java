f535a8463be09fbcb196add1457cff047e922eff
jake0hanna <102441519+jake0hanna@users.noreply.github.com>
2023-03-31 00:50:27 -0400

    Clean up

--- SlideGame/Controller.java
+++ SlideGame/Controller.java
@@ -2,12 +2,10 @@ package SlideGame;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
-public class Controller implements ActionListener, MouseListener
+public class Controller implements ActionListener
 {
 
 	/*
@@ -170,43 +168,5 @@ public class Controller implements ActionListener, MouseListener
 	public UI getGui() {
 		return gui;
 	}
-
-
-
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	
-
-
 
 }
